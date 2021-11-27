01522c945cff73c2e1ff9124aff3e145fde6150b
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2021-11-27 23:58:24 +0000

    Fix typo

--- src/editors/ThemeEditor.tsx
+++ src/editors/ThemeEditor.tsx
@@ -34,7 +34,7 @@ export const ThemeEditor: React.FC<EditorProps> = ({ value, onChange }) => {
   const defaultThemes: SelectableValue[] = [
     { value: 'default', label: 'Default' },
     { value: 'dark', label: 'Dark' },
-    { value: 'light', label: 'light' },
+    { value: 'light', label: 'Light' },
   ];
   return (
     <>
